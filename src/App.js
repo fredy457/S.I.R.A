902ffc30aa3a48dc1f25ae3c254d635b9326b87e
Fredy the coder <62354517+fredy457@users.noreply.github.com>
2024-05-18 22:21:35 -0500

    New Code
    New code

--- src/App.js
+++ src/App.js
@@ -1,7 +1,41 @@
-import './App.css';
+import React, { useState } from 'react';
+import './SlideNavbar.css';
 
-function App() {
- 
-}
+const SlideNavbar = () => {
+  const [isChecked, setIsChecked] = useState(false);
 
-export default App;
+  const toggleCheckbox = () => {
+    setIsChecked(!isChecked);
+  };
+
+  const handleSubmit = e => {                               
+    history.push("/homepage");
+   };
+
+  return (
+    <div className="main">
+      <input type="checkbox" id="chk" aria-hidden="true" checked={isChecked} onChange={toggleCheckbox} />
+
+      <div className="signup">
+        <form>
+          <label htmlFor="chk" aria-hidden="true">Sign up</label>
+          <input type="text" name="txt" placeholder="User name" required="" />
+          <input type="email" name="email" placeholder="Email" required="" />
+          <input type="password" name="pswd" placeholder="Password" required="" />
+          <button>Sign up</button>
+        </form>
+      </div>
+
+      <div className="login">
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="chk" aria-hidden="true">Login</label>
+          <input type="email" name="email" placeholder="Email" required="" />
+          <input type="password" name="pswd" placeholder="Password" required="" />
+          <button>Login</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default SlideNavbar;
